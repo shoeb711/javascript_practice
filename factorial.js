d7721b81eff2a1c5c2a79958749c21c8234388f1
Shoeb Shaikh <shaikhshoeb711@gmail.com>
2026-01-15 20:27:47 +0530

    factorial

---
+++ factorial.js
@@ -0,0 +1,13 @@
+function factorial(num) {
+  if (num < 1) return 1;
+
+  let res = 1;
+
+  for (let i = 1; i <= num; i++) {
+    res = res * i;
+  }
+
+  return res;
+}
+
+console.log(factorial(5));
--- palindrome.js
+++ palindrome.js
@@ -1,13 +1,16 @@
+function palindrome(str) {
+  let rStr = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    rStr += str[i];
+  }
+
+  if (str === rStr) {
+    return "Palindrome";
+  } else {
+    return "not palindrom";
+  }
+}
+
 const str = "racecar";
-
-let rStr = "";
-
-for (let i = str.length - 1; i >= 0; i--) {
-  rStr += str[i];
-}
-
-if (str === rStr) {
-  console.log("Palindrome");
-} else {
-  console.log("not palindrom");
-}
+console.log(palindrome(str));
