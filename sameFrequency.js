81d3d410c064de3c34efdee795b15478742a46a7
Shoeb Shaikh <shaikhshoeb711@gmail.com>
2026-08-08 17:25:00 +0530

    same freq with sqrt

--- sameFrequency.js
+++ sameFrequency.js
@@ -38,5 +38,44 @@ function sameFrequency(num1, num2) {
     return true
 }
 
-const res = sameFrequency(1233, 3211)
+const res = sameFrequency(1233, 3231)
 console.log(res);
+
+
+function sameFreq (arr1,arr2){
+
+    let obj1 = {}
+    let obj2 = {}
+
+
+    for (let num of arr1) {
+        if(obj1.hasOwnProperty(num)){
+            obj1[num] = obj1[num] + 1
+        }else{
+            obj1[num] = 1
+        }
+    }
+
+
+     for (let num of arr2) {
+        if(obj2.hasOwnProperty(num)){
+            obj2[num] = obj2[num] + 1
+        }else{
+            obj2[num] = 1
+        }
+    }
+
+    console.log(obj1)
+    console.log(obj2)
+
+    for(let key in obj1){
+        if(!(obj2.hasOwnProperty(key ** 2)  && obj1[key] === obj2[key**2])){
+            return false
+        }
+    }
+
+    return true
+}
+
+const res = sameFreq([1,2,3],[9,4,1])
+console.log(res)
