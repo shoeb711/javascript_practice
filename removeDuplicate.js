7ff73fe52883f46e29a7b560b64d4ef74f12e007
Shoeb Shaikh <shaikhshoeb711@gmail.com>
2026-09-01 19:27:12 +0530

    remove duplicate

--- removeDuplicate.js
+++ removeDuplicate.js
@@ -1,14 +1,37 @@
-function removeDuplicate(arr) {
-  // return new Set(arr)
-  let res = [];
+// function removeDuplicate(arr) {
+//   // return new Set(arr)
+//   let res = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    if(!res.includes(arr[i])){
-        res.push(arr[i])
+//   for (let i = 0; i < arr.length; i++) {
+//     if(!res.includes(arr[i])){
+//         res.push(arr[i])
+//     }
+//   }
+
+//   return res;
+// }
+
+// console.log(removeDuplicate([1, 12, 3, 1, 50,1,50,9]));
+
+function removeDuplicate(array) {
+    let slow = 0;
+
+    for (let fast = 1; fast < array.length; fast++) {
+        // if (/* fast found a new value */) {
+        if (array[slow] !== array[fast]) {
+            // move slow
+            slow++;
+
+            // put new value at slow
+            array[slow] = array[fast];
+        }
     }
-  }
 
-  return res;
+    // return number of unique values
+    console.log(slow + 1);
+
+    return array.length;
 }
 
-console.log(removeDuplicate([1, 12, 3, 1, 50,1,50,9]));
+// console.log(removeDuplicate([1, 1, 2]));
+console.log(removeDuplicate([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
