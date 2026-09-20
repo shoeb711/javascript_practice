acd9495d27bc3bd475349511f75119ac8077b7cd
Shoeb Shaikh <shaikhshoeb711@gmail.com>
2026-09-20 18:00:40 +0530

    rev twosum

--- twoSum.js
+++ twoSum.js
@@ -1,44 +1,66 @@
+// // function twoSum(array, target) {
+// //   for (let i = 0; i < array.length; i++) {
+// //     for (let j = i + 1; j < array.length; j++) {
+// //       if (array[i] + array[j] === target) {
+// //         return [i, j]
+// //       }
+// //     }
+// //   }
+// // }
+
+// // console.log(twoSum([2,5,5,11], 10));
+
 // function twoSum(array, target) {
+
+//   let obj = {}
+
 //   for (let i = 0; i < array.length; i++) {
-//     for (let j = i + 1; j < array.length; j++) {
-//       if (array[i] + array[j] === target) {
-//         return [i, j]
-//       }
+//     const sum = target - array[i]
+//     if (obj.hasOwnProperty(sum)) {
+
+//       console.log("sum -",sum, "curr -",array[i]);
+
+//       console.log(obj[sum]);
+
+//       return [obj[array[i]], i]
+
 //     }
+
+//     obj[array[i]] = i
+//     // console.log(obj);
+
 //   }
+
 // }
 
-// console.log(twoSum([2,5,5,11], 10));
+function twoSum(nums, target) {
+    // for (let i = 0; i < nums.length; i++) {
+    //     const diff = target - nums[i];
+    //     const diffIndex = nums.indexOf(diff);
 
-function twoSum(array, target) {
+    //     if(diffIndex !== i && nums.includes(diff)) {
+    //         return [nums.indexOf(diff), i]
+    //     }
 
-  let obj = {}
+    //     console.log(diff);
+    // }
 
-  for (let i = 0; i < array.length; i++) {
-    const sum = target - array[i]
-    if (obj.hasOwnProperty(sum)) {
+    // hash map
+    let map = new Map(); 
 
-      console.log("sum -",sum, "curr -",array[i]);
+    for (let i = 0; i < nums.length; i++) {
+        const sum = target - nums[i];
 
-      console.log(obj[sum]);
+        if (map.has(sum)) {
+            return [map.get(sum), i];
+        }
 
-      return [obj[array[i]], i]
-
-
+        map.set(nums[i], i);
     }
-
-    obj[array[i]] = i
-    // console.log(obj);
-
-  }
-
-
-
 }
 
 // console.log(twoSum([6, 1, 2, 3, 4, 5], 4));
-console.log(twoSum([2,7,11,15], 9));
+console.log(twoSum([2, 7, 11, 15], 9));
 // console.log(twoSum([3, 3], 6));
 
 // console.log(twoSum([3, 2, 4], 6));
-
